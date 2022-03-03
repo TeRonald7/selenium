385e663e592137b863134f147cc28cb1cd760150
tom_pro <goodmorning@qq.com>
2022-03-03 16:55:38 +0800

    第三次学习selenium

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def print_hi(name):
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
 
-
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Hello World!")
+
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
