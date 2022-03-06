b584c902e589732804714e792f8786ed019d486e
tom_pro <goodmorning@qq.com>
2022-03-06 15:17:25 +0800

    5

--- demo——001.py
+++ demo——001.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
 sleep(3)
 driver.find_element_by_id("kw").send_keys("selenium")
